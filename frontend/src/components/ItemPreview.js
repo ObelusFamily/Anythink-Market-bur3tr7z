5de6d02c9ce744efe89e952b72321cf6b5893eed
Bob Fong <bobfong3@gmail.com>
2023-01-10 03:07:52 +0800

    Fix:     Frontend UI does not show default image, when user inputs null image path.

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
+import placeholder from "../imgs/placeholder.png";
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
@@ -29,6 +30,8 @@ const ItemPreview = (props) => {
     }
   };
 
+  const fallbackImage = item.image || placeholder;
+
   return (
     <div
       className="card bg-dark border-light p-3"
@@ -37,7 +40,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image}
+        src={fallbackImage}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
